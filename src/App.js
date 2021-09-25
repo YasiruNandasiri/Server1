74b3790c0da37f2d8fb28f4fc94e32de94af690e
YasiruNandasiri <yasiru.naleen@gmail.com>
2021-09-26 03:37:32 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.png';
+import logos from './logos.png';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logos} className="App-logos" alt="logos" />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         I'm Yasiru Nandasiri - 644.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/YN.RHYNN/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow Me on FB
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
